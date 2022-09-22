326f3755691bf4fa3d1b6a57630614b61c9cb084
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2022-09-22 02:09:00 +0100

    addlearners and drawlearners

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 
-let boxUser=[];
+let boxlearners = [];
+let boxdraw = [];
+
+// this function for add learners
 
 function addlearners() {
     
@@ -8,10 +11,11 @@ function addlearners() {
 
     let learners = document.getElementById('add').value;
     if (learners) {
-        boxUser.push(learners)
-        console.log(boxUser);
-        for (i = 0; i < boxUser.length; i++) {
-            res += boxUser[i] +" </br>" ;
+        boxlearners.push(learners)
+        console.log(boxlearners);
+
+        for (i = 0; i < boxlearners.length; i++) {
+            res += boxlearners[i] + "</br>";
         }
     }
     else {
@@ -20,6 +24,33 @@ function addlearners() {
     document.getElementById("userList").innerHTML = res;
 }
 
+// this function for draw learners
+
+function drawlearners () {
+    var randomItem = boxlearners[Math.floor(Math.random() * boxlearners.length)];
+    console.log(randomItem);
+
+}
+
+
+// this function for read learners
+
 function readelearners () {
-    
+    let j;
+    let res = "";
+
+    let tirage = document.getElementById('draw').value;
+    if (tirage) {
+        boxdraw.puch(tirage)
+        console.log(boxdraw);
+
+        for (j = 0; j<boxdraw.length; j++) {
+            res += boxdraw[i] + "</br>";
+        }
+    }
+    else {
+        console.log("???????????");
+    }
+
+    document.getElementById("draw").innerHTML = res;
 }
