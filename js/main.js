bc3693dce5b1b708193d800b23943bfe5a98c2eb
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2022-09-24 00:04:37 +0100

    add api

--- js/main.js
+++ js/main.js
@@ -4,74 +4,138 @@ let boxdraw = [];
 let res = [];
 let randomItem;
 
+// require lybrary
+// const fs = require('fs');
+
+// // create a json object
+// const user = {
+//     "id": 1,
+//     "name": "abdelhaq",
+//     "sujet": "xp"
+// };
+
+// // convert json object to string
+// const data = JSON.stringify(user, null, 2);
+
+// // write json object to string
+// fs.writeFile('http://localhost:7000/data', data, (err) => {
+//     if(err) {
+//         throw err;
+//     }
+//     console.log("json data is saved");
+// });
+
+// // reade json on=bject from file
+// fs.readFile('../data.json', 'utf-8', (err, data) =>  {
+//     if(err) {
+//         throw err;
+//     }
+
+//     // parse Json object
+//     const user = JSON.parse(data.toString());
+
+//     // print sjon object
+//     console.log(user);
+// });
+
+
+
+
 // this function for add learners
-function addlearners() {
-    
-    let i;
-    let res = "";
+const addtopic = async () => {
+    console.log('add');
 
-    let learners = document.getElementById('add').value;
-    if (learners) {
-        boxlearners.push(learners)
-        // console.log(boxlearners);
+    const topic = document.getElementById('add');
+    const taketopic = topic.value;
 
-        for (i = 0; i < boxlearners.length; i++) {
-            res += boxlearners[i] + "</br>";
-        }
-    }
-    else {
-        console.log("???????????");
-    }
-    document.getElementById("userList").innerHTML = res;
+    await axios.post('http://localhost:7000/data', {
+            name: taketopic,
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
+
+// this function for read learners
+const readtopic = async () => {
+    // document.getElementById('draw');
+    
+    await axios.get('http://localhost:7000/data'); {
+        console.log('read');
+    }
+}
+
+
+
+// function addlearners() {
+    
+//     let i;
+//     let res = "";
+
+//     if (learners) {
+//         boxlearners.push(learners)
+//         console.log(boxlearners);
+
+//         for (i = 0; i < boxlearners.length; i++) {
+//             res += boxlearners[i] + "</br>";
+//         }
+//     }
+//     else {
+//         console.log("???????????");
+//     }
+//     document.getElementById("userList").innerHTML = res;
+// }
 
 
 // this function for draw learners
-function drawlearners() {
-    randomItem = boxlearners[Math.floor(Math.random() * boxlearners.length)];
-    draw(randomItem);
-}
+// function drawlearners() {
+//     randomItem = boxlearners[Math.floor(Math.random() * boxlearners.length)];
+//     draw(randomItem);
+// }
 
 
 // delete learners
-function draw (rand) {
-    let i;
+// function draw (rand) {
+//     let i;
     
-    let index = (boxlearners.indexOf(rand));
+//     let index = (boxlearners.indexOf(rand));
 
-    for (i=0; i<boxlearners.length; i++) {
-        if (boxlearners[index] == boxlearners[i]) {
-            res.push(boxlearners[index]) ;
-            boxlearners.splice(index, 1);
-            console.log("t", res);
-        }
-    }
-    let student_create_p = document.createElement("p")
-    const node = document.createTextNode(rand)
-    student_create_p.appendChild(node)
+//     for (i=0; i<boxlearners.length; i++) {
+//         if (boxlearners[index] == boxlearners[i]) {
+//             res.push(boxlearners[index]) ;
+//             boxlearners.splice(index, 1);
+//             console.log("t", res);
+//         }
+//     }
+//     let student_create_p = document.createElement("p")
+//     const node = document.createTextNode(rand)
+//     student_create_p.appendChild(node)
 
-    document.getElementById("draw").append(student_create_p);
-    document.getElementById("userList").innerHTML = boxlearners;
-}
+//     document.getElementById("draw").append(student_create_p);
+//     document.getElementById("userList").innerHTML = boxlearners;
+// }
 
 
 // this function for read learners
-function readelearners () {
-    let j;
-    let res = "";
+// function readelearners () {
+//     let j;
+//     let res = "";
 
-    let tirage = document.getElementById('draw').value;
-    if (tirage) {
-        boxdraw.puch(tirage)
-        console.log(boxdraw);
+//     let tirage = document.getElementById('draw').value;
+//     if (tirage) {
+//         boxdraw.puch(tirage)
+//         console.log(boxdraw);
 
-        for (j = 0; j<boxdraw.length; j++) {
-            res += boxdraw[i] + "</br>";
-        }
-    }
-    else {
-        console.log("???????????");
-    }
+//         for (j = 0; j<boxdraw.length; j++) {
+//             res += boxdraw[i] + "</br>";
+//         }
+//     }
+//     else {
+//         console.log("???????????");
+//     }
 
-    document.getElementById("draw").innerHTML = res;
-}
+//     document.getElementById("draw").innerHTML = res;
+// }
