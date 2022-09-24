f6327a930f48d3f8118d5aea966d4399703c166f
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2022-09-24 23:09:25 +0100

    reade data of a innerhtml the student

--- js/main.js
+++ js/main.js
@@ -4,42 +4,6 @@ let boxdraw = [];
 let res = [];
 let randomItem;
 
-// require lybrary
-// const fs = require('fs');
-
-// // create a json object
-// const user = {
-//     "id": 1,
-//     "name": "abdelhaq",
-//     "sujet": "xp"
-// };
-
-// // convert json object to string
-// const data = JSON.stringify(user, null, 2);
-
-// // write json object to string
-// fs.writeFile('http://localhost:7000/data', data, (err) => {
-//     if(err) {
-//         throw err;
-//     }
-//     console.log("json data is saved");
-// });
-
-// // reade json on=bject from file
-// fs.readFile('../data.json', 'utf-8', (err, data) =>  {
-//     if(err) {
-//         throw err;
-//     }
-
-//     // parse Json object
-//     const user = JSON.parse(data.toString());
-
-//     // print sjon object
-//     console.log(user);
-// });
-
-
-
 
 // this function for add learners
 const addtopic = async () => {
@@ -62,39 +26,25 @@ const addtopic = async () => {
 // this function for read learners
 const readtopic = async () => {
 
+    let t = "";
+    let j;
+
     const res = await axios.get('http://localhost:7000/data'); {
         const data = await res.data;
 
         if (data) {
-            boxlearners = data;
-                
+            console.log(data);
+            data.forEach(ele=>{
+                boxlearners.push(ele.name);
+                // console.log("box", boxlearners);
+            })
         }
+        for (j = 0; j < boxlearners.length; j++) {
+            t += boxlearners[j] + "</br>";
+        }
+        document.getElementById("userList").innerHTML = t;
     }
-    boxlearners.forEach(ele=>{
-       document.getElementById("draw").innerHTML = ele.name;
-    })
 }
-
-
-
-// function addlearners() {
-    
-//     let i;
-//     let res = "";
-
-//     if (learners) {
-//         boxlearners.push(learners)
-//         console.log(boxlearners);
-
-//         for (i = 0; i < boxlearners.length; i++) {
-//             res += boxlearners[i] + "</br>";
-//         }
-//     }
-//     else {
-//         console.log("???????????");
-//     }
-//     document.getElementById("userList").innerHTML = res;
-// }
 
 
 // this function for draw learners
