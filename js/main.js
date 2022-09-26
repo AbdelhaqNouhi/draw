774f2badd6780bd99c2381e4565e228590d1c1ad
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2022-09-26 12:34:14 +0100

    add randem function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 let boxlearners = [];
-let boxdraw = [];
+let boxsujet = [];
 let res = [];
 let randomItem;
 
@@ -8,41 +8,38 @@ let randomItem;
 
 // this function for add learners
 const addtopic = async () => {
-    console.log('add');
 
     const topic = document.getElementById('addtopic');
     const taketopic = topic.value;
 
-    await axios.post('http://localhost:7000/data', {
-            name: taketopic,
-        })
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    if (taketopic) {
+        await axios.post('http://localhost:7000/data', {
+                name: taketopic,
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+    else {
+        console.log("error");
+    }
+
 }
 
 // this function for read learners
 const readtopic = async () => {
-
-    let t = "";
-    let j;
-
     const res = await axios.get('http://localhost:7000/data'); {
         const data = await res.data;
 
         if (data) {
-            // console.log(data);
             data.forEach(ele=>{
                 boxlearners.push(ele.name);
+                document.getElementById("userList").innerHTML = boxlearners;
             })
         }
-        for (j = 0; j < boxlearners.length; j++) {
-            t += boxlearners[j] + "</br>";
-        }
-        document.getElementById("userList").innerHTML = t;
     }
 }
 
@@ -54,11 +51,8 @@ const addsujet = async () => {
     const takesujet = sujet.value
     console.log(takesujet);
 
-    const res = await fetch('http://localhost:7000/sujet', {
-        method: 'POST',
-        dody: JSON.stringify({
+    const res = await axios.post('http://localhost:7000/sujet', {
             name: takesujet,
-        }),
     });
     const data = await res.json();
     if (data) {
@@ -69,36 +63,53 @@ const addsujet = async () => {
     }
 }
 
+const readsujet = async () => {
+    const res = await axios.get('http://localhost:7000/sujet'); {
+        const data = await res.data;
+        
+        if (data) {
+            data.forEach(ele => {
+                boxsujet.push(ele.name);
+                document.getElementById("sujetlist").innerHTML = boxsujet;
+            })
+        }
+    }
+} 
+
 
 
 // this function for draw learners
-function drawtopic() {
-    const randomItem = boxlearners[Math.floor(Math.random() * boxlearners.length)];
-    showdrawtopic(randomItem)
+function Randem() {
+    const randomuser = boxlearners[Math.floor(Math.random() * boxlearners.length)];
+    const randomsujet = boxsujet[Math.floor(Math.random() * boxlearners.length)];
+    console.log('users', randomuser);
+    console.log('sujet', randomsujet);
+
+    
 }
 
 
 // delete learners
-function showdrawtopic (rand) {
+// function showdrawtopic (rand) {
 
-    let i;
+//     let i;
     
-    let index = (boxlearners.indexOf(rand));
+//     let index = (boxlearners.indexOf(rand));
     
-    for (i=0; i<boxlearners.length; i++) {
-        if (boxlearners[index] == boxlearners[i]) {
-            // console.log('index', boxlearners[index]);
-            // console.log('box', boxlearners[i]);
-            boxlearners.splice(index, 1);
-            res.push(boxlearners[index]) ;
-            console.log("t", res);
-            // console.log(boxlearners);
-        }
-    }
+//     for (i=0; i<boxlearners.length; i++) {
+//         if (boxlearners[index] == boxlearners[i]) {
+//             // console.log('index', boxlearners[index]);
+//             // console.log('box', boxlearners[i]);
+//             boxlearners.splice(index, 1);
+//             res.push(boxlearners[index]) ;
+//             console.log("t", res);
+//             // console.log(boxlearners);
+//         }
+//     }
     
-    let student_create_p = document.createElement("p")
-    const node = document.createTextNode(rand)
-    student_create_p.appendChild(node)
+//     let student_create_p = document.createElement("p")
+//     const node = document.createTextNode(rand)
+//     student_create_p.appendChild(node)
     
-    document.getElementById("draw").append(student_create_p);
-}
+//     document.getElementById("draw").append(student_create_p);
+// }
